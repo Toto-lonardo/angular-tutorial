bcdbace5a5d87b3be8f9775d24dd08af6e9f4239
Toto-lonardo <s.lonardo87@gmail.com>
2023-04-20 01:15:20 +0200

    Adding fake server

---
+++ src/app/services/prenotazioni.service.ts
@@ -0,0 +1,15 @@
+import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrenotazioniService {
+
+  constructor(private _http: HttpClient ) { }
+
+  addPrenotazione(data: any): Observable<any> {
+    return this._http.post('http://localhost:3000/prenotazioni', data);
+  }
+}
